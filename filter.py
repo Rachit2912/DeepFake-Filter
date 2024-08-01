195084e3dc9760cfd5792e96835e42f9904bfa87
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2024-08-01 17:50:33 +0000

    updated

--- filter.py
+++ filter.py
@@ -2,3 +2,11 @@ import cv2 as cv
 import dlib as dl
 import numpy as np 
 
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
+predictor = dl.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+celeb_img = 
+cele_img_gray = 
+
+
